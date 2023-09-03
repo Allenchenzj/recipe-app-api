271bf35406a576d2997a554be739b87cffc406b8
Allen <chenzhijiemel@gmail.com>
2023-09-03 10:59:59 +1000

    fix bug

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,14 +6,16 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    """Test models"""
-    email = 'test@example.com'
-    password = 'testpass123'
-    user = get_user_model().objects.create_user(
-        email=email,
-        password=password,
-    )
 
-    self.assertEqual(user.email, email)
-    self.assertTrue(user.check_passwords(password))
+    def test_create_user_with_email_successful(self):
+        """Test models"""
+        email = 'test1@example.com'
+        password = 'testpass1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
 
